d39a0154f8e103705c061c0e8defcb004e782e18
isaac <isaac.gutierrez@ksms.mx>
2024-03-07 14:57:05 -0600

    fisrt try

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+import { useState } from "react";
+import WebSocket from "./websocket";
 
 function App() {
+    const [stompClient, setStompClient] = useState(null);
+    const [stompResponse, setStompResponse] = useState(null);
+const subscribe = async (url) => {
+
+
+    const token = "a24275df-70db-4d06-89ed-d5447d070c29"
+
+    const callback = async (message) => {
+        let response = JSON.parse(message.body);
+        setStompResponse(response);
+    };
+    let newStompClient=null
+    try {
+
+        let socket = new SockJS(`ws://localhost:8000/ws/img/inference`);
+        newStompClient = Stomp.over(socket);
+        newStompClient.debug = null; // COMENTAR PARA DESACTIVAR COMENTARIOS DEL WEBSOCKET EN CONSOLA
+        setStompClient(newStompClient);
+        WebSocket().subscribeService(newStompClient, url, callback, token);
+
+        socket.onclose = async () => {
+            await subscribe(url, token);
+        };
+        // setReconnectStomp(false);
+        // setTryActiveRequests(true);
+    } catch (error) {
+        console.error("error stomp client", error);
+    }
+};
+
+const send_message = async (newStompClient, json) => {
+
+    if (newStompClient) {
+        WebSocket().clientSend(newStompClient, `ws://localhost:8000/ws/img/inference`, json,
+            {
+                "Api-Key": "a24275df-70db-4d06-89ed-d5447d070c29",
+            })
+        return newStompClient
+    }
+};
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<h1>Image Upload</h1>
+    <form id="uploadForm" encType="multipart/form-data">
+        <input type="file" id="fileInput" name="image" accept="image/*"/>
+        <button type="submit">Upload Image</button>
+    </form>
+    <div id="imagePreview"></div>
+    <h1>WebSocket Connection</h1>
+    <button onClick={subscribe}>Connect to socket</button>
+    <button onClick={send_message}>Send message to web socket</button>
+    <div id="executionTime_conn"></div>
+    <div id="executionTime_"></div>
+    <div id="response"></div>
       </header>
     </div>
   );
